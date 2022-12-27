8a01cf57a1c4655299e0b442969c00fa4e948b5b
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-12-27 15:08:04 +0530

    profile screen

---
+++ src/screen/ProfileScreen.js
@@ -0,0 +1,47 @@
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+import './css/Profile.css';
+
+const ProfileScreen = () => {
+
+    const navigate = useNavigate()
+
+    const user = JSON.parse(localStorage.getItem('active'));
+    const profile = JSON.parse(localStorage.getItem(`profile_${user}`));
+
+    useEffect(() => {
+        if (!profile?.username) {
+            navigate('/')
+        }
+    }, [profile])
+    
+
+    return (
+        <>
+            <Navbar />
+            <div className='profile__container'>
+                <div className='activeProfile__box'>
+                    <div className='profile__image'>
+                        <img src={profile?.profile} alt="profile" />
+                    </div>
+                    <h2>{profile?.username}</h2>
+                    <div className='profile__grid'>
+                        <div className='profile__boxx'>
+                            <div><span>Email :</span> {profile?.email}</div>
+                            <div><span>Phone No :</span> {profile?.phone_no}</div>
+                            <div><span>Date of Birth :</span> {profile?.birth_date}</div>
+                        </div>
+                        <div className='profile__boxx'>
+                            <div><span>City :</span> {profile?.city}</div>
+                            <div><span>District :</span> {profile?.district}</div>
+                            <div><span>Pincode :</span> {profile?.pincode}</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ProfileScreen
