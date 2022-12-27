d3a1320e11f5d0556df7b6d9900555e4fbe98d3e
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-12-27 15:13:46 +0530

    Landing screen

---
+++ src/screen/LandingScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../components/Header'
+import Navbar from '../components/Navbar'
+
+const LandingScreen = () => {
+    return (
+        <div>
+            <Navbar />
+            <Header />
+        </div>
+    )
+}
+
+export default LandingScreen;
