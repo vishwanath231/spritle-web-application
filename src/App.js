597353a239a2789844c659f7067c93ffec70ab34
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-12-27 15:15:29 +0530

    react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react'
-import Login from './screen/Login';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Landing from './screen/Landing';
-import Register from './screen/Register';
-import Home from './screen/Home';
 import './App.css';
-import About from './screen/About';
-import ProfileForm from './screen/ProfileForm';
+import AboutScreen from './screen/AboutScreen';
+import HomeScreen from './screen/HomeScreen';
+import LandingScreen from './screen/LandingScreen';
+import LoginScreen from './screen/LoginScreen';
+import ProfileFormScreen from './screen/ProfileFormScreen';
+import RegisterScreen from './screen/RegisterScreen';
 import ProfileScreen from './screen/ProfileScreen';
 
 const App = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<Landing /> } />
-                <Route path='/about' element={<About /> } />
-                <Route path='/login' element={<Login /> } />
-                <Route path='/register' element={<Register /> } />
-                <Route path='/profile' element={<ProfileScreen /> } />
-                <Route path='/user' element={<ProfileForm /> } />
-                <Route path='/home' element={<Home /> } />
+                <Route path='/' element={<LandingScreen /> } />
+                <Route path='/about' element={<AboutScreen /> } />
+                <Route path='/login' element={<LoginScreen /> } />
+                <Route path='/register' element={<RegisterScreen /> } />
+                <Route path='/profile' element={ <ProfileScreen /> } />
+                <Route path='/user' element={<ProfileFormScreen /> } />
+                <Route path='/home' element={<HomeScreen /> } />
             </Routes>
         
         </BrowserRouter>
